14f6994f6946fe88f5a65eecc589bc681755be09
C-Lee-Hamilton <122707378+C-Lee-Hamilton@users.noreply.github.com>
2023-03-07 16:33:47 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import {Cards} from './components/Cards'
+
+function App() {
+
+
+  return (
+    <div className="App">
+    
+      <h2>Pokemon TCG Open Packs</h2>
+      Ouvrez les cartes
+      <br/>
+      <Cards/>
+    
+    </div>
+
+  );
+}
+
+export default App;
